11c2dac6bc1651eb7eab1a6b39ce083364ef1fe5
Bjorn Yourey <byourey7783@gmail.com>
2021-02-21 14:45:36 -0500

    corrector with questions

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ const fs = require('fs');
 inquirer
   .prompt([
       {
-          type: "input",
-          message: "What is the name of your project"?
-          name: "title",
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
       },
       {
-        type: "input",
-        message: "Please enter a description of your project",
-        name: "description",
+        type: 'input',
+        message: 'Please enter a description of your project',
+        name: 'description',
       },
       {
-        type: "input",
-        message: "What are the installation instructions for your project"?
-        name: "installation",  
+        type: 'input',
+        message: 'What are the installation instructions for your project?',
+        name: 'installation',  
       },
       {
-        type: "input",
-        message: "Who do you credit this work to"?
-        name: "credit",
+        type: 'input',
+        message: 'Who do you credit this work to?',
+        name: 'credit',
       },
       {
-        type: "checkbox",
-        message: "Please choose a license",
-        name: "license",
+        type: 'checkbox',
+        message: 'Please choose a license',
+        name: 'license',
         choices: [
             'Apache 2.0',
             'GNU',
@@ -36,19 +36,19 @@ inquirer
         ]
       },
       {
-        type: "input",
-        message: "Guidelines for other developers to contribute.",
-        name: "contributing",
+        type: 'input',
+        message: 'Guidelines for other developers to contribute.',
+        name: 'contributing',
       },
       {
-        type: "input",
-        message: "What is your Github username"?
-        name: "username",
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username',
       },
       {
-        type: "input",
-        message: "What is your email address"?
-        name: "email",
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
       },
            
   ])
