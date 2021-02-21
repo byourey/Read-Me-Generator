ab357dd5005474fff24c3c58e9e544ca0f2ee9c8
Bjorn Yourey <byourey7783@gmail.com>
2021-02-21 15:06:59 -0500

    readme

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const requiredQuestion = async (input) => {
-    if (input === "") {
-       return 'This question is required';
-    }
-    return true;
-  };
+
 
 // Function created to ask questions
 inquirer
@@ -14,25 +9,21 @@ inquirer
         type: 'input',
         message: 'What is the name of your project?',
         name: 'title',
-        validate: requiredQuestion,
       },
       {
         type: 'input',
         message: 'Please enter a description of your project',
         name: 'description',
-        validate: requiredQuestion,
       },
       {
         type: 'input',
         message: 'What are the installation instructions for your project?',
         name: 'installation',
-        validate: requiredQuestion,
       },
       {
         type: 'input',
         message: 'Who do you credit this work to?',
         name: 'credit',
-        validate: requiredQuestion,
       },
       {
         type: 'checkbox',
@@ -44,32 +35,28 @@ inquirer
             'ISC',
             'MIT',
         ],
-        validate: requiredQuestion,
       },
       {
         type: 'input',
         message: 'Guidelines for other developers to contribute.',
         name: 'contributing',
-        validate: requiredQuestion,
       },
       {
         type: 'input',
         message: 'What is your Github username?',
         name: 'username',
-        validate: requiredQuestion,
       },
       {
         type: 'input',
         message: 'What is your email address?',
         name: 'email',
-        validate: requiredQuestion,
       },
            
   ])
 
   .then((answers) => {
 
-    const readMeContent = `
+    const readMe = `
     # ${answers.project_name}
 
   ## Table of Contents
@@ -97,7 +84,7 @@ inquirer
      ${answers.contributing}
   ## Tests
         How to run tests:
-        ${answers.test_instructions}
+        ${answers.test}
 
   ## Questions
      *For futher questions check out my github profile:
@@ -108,7 +95,7 @@ inquirer
      `;
   
 
-    fs.writeFile('read-me.md', readMeContent, (err) =>
+    fs.writeFile('read-me.md', readMe, (err) =>
                 err ? console.log(err) : console.log('Success!')
             );
         });
